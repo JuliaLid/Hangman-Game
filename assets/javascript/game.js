45652665432b81dea304100acdc831099192d694
Julia Lidstrom <juleslidstrom@gmail.com>
2017-12-03 12:59:51 -0600

    Updating JS file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 //Declare global variables
-var words = ["pelican", "calhoun","harriet","nokomis","mantrap"]; 
+var words = ["pelican", "calhoun","harriet","nokomis","mantrap","minnetonka","itasca","vermillion"]; 
 var guessCount = 12;									
 var wins = 0;												
 var answerArray = [];
-var wrongGuess =  [];
+var wrongGuess = []; 
 var wordChoice;
+var remainingLetters;
 
 // function to initalize random word selection
 function selectWord() {
@@ -12,8 +13,9 @@ function selectWord() {
 	for (var i=0; i < wordChoice.length; i++) {
 		answerArray [i] = "_ ";
 	}	
-	document.getElementById("hold").innerHTML=answerArray;
+	document.getElementById("hold").innerHTML=(answerArray.join(" "));
 	document.getElementById("guessCounter").innerHTML=guessCount;
+	console.log(wordChoice);
 }
 
 //call selectWord funciton on window load
@@ -24,21 +26,99 @@ document.onkeyup = function(event) {
 	
 	var remainingLetters = wordChoice.length;
 
-	while(remainingLetters>0 && guessCount>0) {
+	// while(remainingLetters>0 && guessCount>0) {
 		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
+
+		//working loop
+		// for (var j=0; j<wordChoice.length; j++){
+		// 		if (wordChoice[j] === userInput){
+		// 		answerArray[j] = userInput;
+		// 		document.getElementById("hold").innerHTML=(answerArray.join(" "));
+		// 	} else if (wordChoice.indexOf(userInput)===-1){
+		// 		wrongGuess.push(userInput);
+		// 		console.log(wrongGuess)
+		// 		// console.log(wrongGuess.slice(-1));
+		// 		document.getElementById("letters").innerHTML = wrongGuess[0];userInput +", ";
+		// 	} 
+		// };
+
 		for (var j=0; j<wordChoice.length; j++){
-			if (wordChoice[j] === userInput){
-				answerArray[j].push(userInput);
-				document.getElementById("hold").innerHTML=answerArray;
-			} else if (wrongGuess.indexOf(userInput)!== 1){
-				wrongGuess.push(userInput);
-				document.getElementById("letters").innerHTML = userInput + ", "; 
-			} else {
-				wrongGuess += wrongGuess;
-			}
+					if (wordChoice[j] === userInput){
+					answerArray[j] = userInput;
+					document.getElementById("hold").innerHTML=(answerArray.join(" "));
+					} 
+				};
+
+		if ((wrongGuess.indexOf(userInput)===-1) && (wordChoice.indexOf(userInput)===-1)){
+			wrongGuess.push(userInput);
+			document.getElementById("letters").innerHTML = wrongGuess +",   ";
 		}
-		remainingLetters--;
-		guessCount--;
-		document. getElementById("guessCounter").innerHTML = 	guessCount
-	}
-};
+	};
+		// if (wordChoice.indexOf(userInput)===-1){
+		// 				wrongGuess.push(userInput);
+		// 				// wrongGuess = wordChoice[j];
+		// 				console.log(wrongGuess)
+		// 				// console.log(wrongGuess.slice(-1));
+		// 				document.getElementById("letters").innerHTML = wrongGuess +",   ";
+		// 			} 
+		// 		};
+	
+		// for (var k=0; k<wrongGuess.length; k++){
+		// 	if(wrongGuess.indexOf(userInput)!==-1){
+		// 		var wrongLetter = wrongGuess[k];
+		// 		console.log(typeof(wrongLetter));
+		// 		document.getElementById("letters").innerHTML = wrongLetter +", ";
+		// 	}
+		// 	else {
+		// 		console.log(wrongGuess);
+		// 	}
+		// }
+		// function eliminateDuplicates(wrongGuess) {
+		// 	 var out = [];
+		// 	 var obj = {};
+		// 	 for (var k=0; k<wrongGuess.length; k++) {
+		// 	 	obj[wrongGuess[k]]=0;
+		// 	 }
+		// 	 for (k in obj){
+		// 	 	out.push(k);
+		// 	 }
+		// 	return out;
+		// }
+
+		//
+	// 	// }
+	// 	remainingLetters--;
+	// 	guessCount--;
+	// 	// document.getElementById("letters").innerHTML = (wrongGuess.join(","));
+	// 	document. getElementById("guessCounter").innerHTML = guessCount
+	// }
+	// endGame(); ////find a place to call the function
+// };
+//create an end game function
+// function endGame() {
+// 	console.log("endGame");
+// 	if (remainingLetters===0 && guessCount >=0) {
+// 		wins ++
+// 		document. getElementById("winCounter").innerHTML = 	wins;
+// 	} else {
+// 		document. getElementById("winCounter").innerHTML = 	"Sorry, try again!";
+// 	}
+// };
+
+// //if it's a win, then use answerArray to remove eit, if not it can stay to be guessed again
+// //call end game function
+
+
+// // game reset function
+
+// function gameReset(){
+// 	//remove randomly generated word from 
+// 	// if(words.indexOf(answerArray) > -1) {
+// 	// 	Words.splice(wordChoice,1)
+// 	// }
+// 	wrongGuess = [];
+// 	answerArray = [];
+// 	selectWord();
+// }
+
+// gameReset();
